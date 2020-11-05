63bf94d153e94dcf03aac7fd8d78d13162bee9c1
Michał Frankiewicz <michal@frankiewicz.me>
2020-11-05 08:19:35 +0100

    fixes

--- common.sh
+++ common.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd ..
+git clone git@github.com:wiosna-dev/jmeter.git ../jmeter
 
-git clone git@github.com:wiosna-dev/jmeter.git
+cp ../jmeter/jmeter5.2/bin/jmeter.dist ../jmeter/jmeter5.2/bin/jmeter
 
-cp jmeter/jmeter5.2/bin/jmeter.dist jmeter/jmeter5.2/bin/jmeter
+cp ../jmeter/jmeter5.2/bin/jmeter.properties.dist ../jmeter/jmeter5.2/bin/jmeter.properties
 
-cp jmeter/jmeter5.2/bin/jmeter.properties.dist jmeter/jmeter5.2/bin/jmeter.properties
+cp ../jmeter/jmeter5.2/bin/system.properties.dist ../jmeter/jmeter5.2/bin/system.properties
 
-cp jmeter/jmeter5.2/bin/system.properties.dist jmeter/jmeter5.2/bin/system.properties
-
-echo "server.rmi.ssl.disable=true" >> jmeter/jmeter5.2/bin/jmeter.properties
+echo "server.rmi.ssl.disable=true" >> ../jmeter/jmeter5.2/bin/jmeter.properties
--- master.sh
+++ master.sh
@@ -5,3 +5,5 @@ apt update && apt install -y openjdk-8-jdk python3 python3-tk python3-pip python
 python3 -m pip install bzt
 
 source common.sh
+
+echo "java.rmi.server.hostname=10.114.0." >> ../jmeter/jmeter5.2/bin/jmeter.properties
