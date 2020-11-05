34d48e58fe44005547f78a9ea354f1ed74512463
Michał Frankiewicz <michal@frankiewicz.me>
2020-11-05 08:17:01 +0100

    adds script

--- common.sh
+++ common.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+cd ..
+
 git clone git@github.com:wiosna-dev/jmeter.git
+
+cp jmeter/jmeter5.2/bin/jmeter.dist jmeter/jmeter5.2/bin/jmeter
+
+cp jmeter/jmeter5.2/bin/jmeter.properties.dist jmeter/jmeter5.2/bin/jmeter.properties
+
+cp jmeter/jmeter5.2/bin/system.properties.dist jmeter/jmeter5.2/bin/system.properties
+
+echo "server.rmi.ssl.disable=true" >> jmeter/jmeter5.2/bin/jmeter.properties
