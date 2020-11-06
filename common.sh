b0bb6413dfaad7071884cbe706b94b888035338d
Michał Frankiewicz <michal@frankiewicz.me>
2020-11-06 12:57:40 +0100

    extends common part

--- common.sh
+++ common.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt update && apt install -y openjdk-8-jdk net-tools glances
+
 IP=$(ifconfig | grep -Eo 'inet 10\.114\.0\.[0-9]{1,3}' | grep -o [0-9].*)
 RAM=$(cat /proc/meminfo | grep MemTotal | grep -o '[[:digit:]]*')
 RAM=$(($RAM/1024/1024))
--- slave.sh
+++ slave.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-apt-get update && apt install -y openjdk-8-jdk net-tools glances
-
 source common.sh
