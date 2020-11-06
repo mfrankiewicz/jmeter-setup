d791b0ef784e79db56b4ada499350c53fae42e34
Michał Frankiewicz <michal@frankiewicz.me>
2020-11-06 12:41:43 +0100

    adds resolving ip and ram

--- common.sh
+++ common.sh
@@ -9,3 +9,7 @@ cp ../jmeter/jmeter5.2/bin/jmeter.properties.dist ../jmeter/jmeter5.2/bin/jmeter
 cp ../jmeter/jmeter5.2/bin/system.properties.dist ../jmeter/jmeter5.2/bin/system.properties
 
 echo "server.rmi.ssl.disable=true" >> ../jmeter/jmeter5.2/bin/jmeter.properties
+
+IP=$(ifconfig | grep -Eo 'inet 10\.114\.0\.[0-9]{1,3}' | grep -o [0-9].*)
+RAM=$(cat /proc/meminfo | grep MemTotal | grep -o '[[:digit:]]*')
+RAM=$(($RAM/1024/1024))
