0caf242c8630c3f23e2a55d16f3797f697a13a67
Michał Frankiewicz <michal@frankiewicz.me>
2020-11-06 12:49:50 +0100

    adds setting ip for slave

--- common.sh
+++ common.sh
@@ -15,3 +15,5 @@ cp ../jmeter/jmeter5.2/bin/system.properties.dist ../jmeter/jmeter5.2/bin/system
 echo "server.rmi.ssl.disable=true" >> ../jmeter/jmeter5.2/bin/jmeter.properties
 
 echo "java.rmi.server.hostname=${IP}" >> ../jmeter/jmeter5.2/bin/system.properties
+
+sed -i "s/#RMI_HOST_DEF=-Djava.rmi.server.hostname=xxx.xxx.xxx.xxx/RMI_HOST_DEF=-Djava.rmi.server.hostname=${IP}/g" ../jmeter/jmeter5.2/bin/jmeter-server
